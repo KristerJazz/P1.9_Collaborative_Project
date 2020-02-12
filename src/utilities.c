9b0042bd29b2c20805fd40be240edbaebc74810b
Mattia Carello <matrixc@dhcp-129-039.wifi.sissa.it>
2020-02-12 18:56:11 +0100

    utilities files fix typos

--- src/utilities.c
+++ src/utilities.c
@@ -1,10 +1,10 @@
-A #include<stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 
-    /* helper function: read a line and then return
-       the first string with whitespace stripped off */
-    static int get_a_line(FILE *fp, char *buf) {
+/* helper function: read a line and then return
+   the first string with whitespace stripped off */
+int get_a_line(FILE *fp, char *buf) {
   char tmp[BLEN], *ptr;
 
   /* read a line and cut of comments and blanks */
@@ -34,7 +34,7 @@ A #include<stdio.h>
 }
 
 /* helper function: zero out an array */
-static void azzero(double *d, const int n) {
+void azzero(double *d, const int n) {
   int i;
   for (i = 0; i < n; ++i) {
     d[i] = 0.0;
@@ -42,7 +42,7 @@ static void azzero(double *d, const int n) {
 }
 
 /* helper function: apply minimum image convention */
-static double pbc(double x, const double boxby2) {
+double pbc(double x, const double boxby2) {
   while (x > boxby2) x -= 2.0 * boxby2;
   while (x < -boxby2) x += 2.0 * boxby2;
   return x;
