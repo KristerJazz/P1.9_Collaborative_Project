9a63bf5c6263d8e08e999cd087cd97b869384e9e
Luis Leon <lleon95@gmail.com>
2020-02-12 18:14:48 +0100

    Add the velverlet main file

---
+++ src/velverlet.c
@@ -0,0 +1,36 @@
+/*
+  P1.9_seed
+  Function: Velverlet Time Integration (velverlet())
+  Contributor: Luis Leon
+
+  Dependencies:
+  - Struct: mdsys_t
+  - Consts: mvsq2e (m*v^2 in kcal/mol)
+*/
+
+#include "../include/ljmd.h"
+
+void velverlet(mdsys_t *sys)
+{
+    int i;
+
+    /* first part: propagate velocities by half and positions by full step */
+    for (i=0; i<sys->natoms; ++i) {
+        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
+        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
+        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        sys->rx[i] += sys->dt*sys->vx[i];
+        sys->ry[i] += sys->dt*sys->vy[i];
+        sys->rz[i] += sys->dt*sys->vz[i];
+    }
+
+    /* compute forces and potential energy */
+    force(sys);
+
+    /* second part: propagate velocities by another half step */
+    for (i=0; i<sys->natoms; ++i) {
+        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
+        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
+        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+    }
+}
