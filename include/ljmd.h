1334e0f6074065af1a593b758a7887a201e02e9d
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-02-12 18:54:08 +0100

    Fix static qualifier

--- include/ljmd.h
+++ include/ljmd.h
@@ -45,32 +45,32 @@ const double mvsq2e = 2390.05736153349;
  * @brief helper function: read a line and then return
  *  the first string with whitespace stripped off
  */
-static int get_a_line(FILE *fp, char *buf);
+int get_a_line(FILE *fp, char *buf);
 
 /**
  * @brief helper function: zero out an array
  */
-static void azzero(double *d, const int n);
+void azzero(double *d, const int n);
 
 /**
  * @brief helper function: apply minimum image convention
  */
-static double pbc(double x, const double boxby2);
+double pbc(double x, const double boxby2);
 
 /**
  * @brief compute kinetic energy
  */
-static void ekin(mdsys_t *sys);
+void ekin(mdsys_t *sys);
 
 /**
  * @brief compute forces
  */
-static void force(mdsys_t *sys);
+void force(mdsys_t *sys);
 
 /**
  * @brief velocity verlet
  */
-static void velverlet(mdsys_t *sys);
+void velverlet(mdsys_t *sys);
 
 /**
  * @brief propagate positions
@@ -85,6 +85,6 @@ void propagate_velocity(mdsys_t *sys, int i);
 /**
  * @brief append data to output.
  */
-static void output(mdsys_t *sys, FILE *erg, FILE *traj);
+void output(mdsys_t *sys, FILE *erg, FILE *traj);
 
 #endif
