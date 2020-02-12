4850d2e178929e7e0e9c43639798d7d4e007df8a
Luis Leon <lleon95@gmail.com>
2020-02-12 18:23:49 +0100

    Update the velverlet main function to use step functions

--- src/velverlet.c
+++ src/velverlet.c
@@ -16,12 +16,8 @@ void velverlet(mdsys_t *sys)
 
     /* first part: propagate velocities by half and positions by full step */
     for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
-        sys->rx[i] += sys->dt*sys->vx[i];
-        sys->ry[i] += sys->dt*sys->vy[i];
-        sys->rz[i] += sys->dt*sys->vz[i];
+        propagate_position(sys, i);
+        propagate_velocity(sys, i);
     }
 
     /* compute forces and potential energy */
@@ -29,8 +25,8 @@ void velverlet(mdsys_t *sys)
 
     /* second part: propagate velocities by another half step */
     for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        propagate_velocity(sys, i);
+        propagate_velocity(sys, i);
+        propagate_velocity(sys, i);
     }
 }
