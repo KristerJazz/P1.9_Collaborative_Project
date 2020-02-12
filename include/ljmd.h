aae0575ef194ea56a919a1b88897d494c00e7d84
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-02-12 18:30:00 +0100

    Fix style (clang-format --style=google)

--- include/ljmd.h
+++ include/ljmd.h
@@ -1,41 +1,40 @@
 #ifndef __LJMD__
 #define __LJMD__
 
-/** 
- * @brief structure to hold the complete information 
- * about the MD system 
+/**
+ * @brief structure to hold the complete information
+ * about the MD system
  */
 struct _mdsys {
-    int natoms,nfi,nsteps;
-    double dt, mass, epsilon, sigma, box, rcut;
-    double ekin, epot, temp;
-    double *rx, *ry, *rz;
-    double *vx, *vy, *vz;
-    double *fx, *fy, *fz;
+  int natoms, nfi, nsteps;
+  double dt, mass, epsilon, sigma, box, rcut;
+  double ekin, epot, temp;
+  double *rx, *ry, *rz;
+  double *vx, *vy, *vz;
+  double *fx, *fy, *fz;
 };
 typedef struct _mdsys mdsys_t;
 
 /**
- * @brief boltzman constant in kcal/mol/K 
+ * @brief boltzman constant in kcal/mol/K
  */
-const double kboltz=0.0019872067;     
+const double kboltz = 0.0019872067;
 
 /**
  * @brief conversion constant: m*v^2 in kcal/mol
  */
-const double mvsq2e=2390.05736153349;
+const double mvsq2e = 2390.05736153349;
 
 /**
  * @brief helper function: read a line and then return
- *  the first string with whitespace stripped off 
+ *  the first string with whitespace stripped off
  */
 static int get_a_line(FILE *fp, char *buf);
 
 /**
- * @brief helper function: zero out an array 
+ * @brief helper function: zero out an array
  */
 static void azzero(double *d, const int n);
-
 
 /**
  * @brief helper function: apply minimum image convention
@@ -43,19 +42,17 @@ static void azzero(double *d, const int n);
 static double pbc(double x, const double boxby2);
 
 /**
- * @brief compute kinetic energy 
+ * @brief compute kinetic energy
  */
 static void ekin(mdsys_t *sys);
 
-
 /**
- * @brief compute forces 
+ * @brief compute forces
  */
 static void force(mdsys_t *sys);
 
-
 /**
- * @brief velocity verlet 
+ * @brief velocity verlet
  */
 static void velverlet(mdsys_t *sys);
 
@@ -69,9 +66,8 @@ void propagate_position(mdsys_t *sys, int i);
  */
 void propagate_velocity(mdsys_t *sys, int i);
 
-
 /**
- * @brief append data to output. 
+ * @brief append data to output.
  */
 static void output(mdsys_t *sys, FILE *erg, FILE *traj);
 
