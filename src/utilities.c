7e9213f3d4074c62b06ca7614bdd7963b327a384
Mattia Carello <matrixc@dhcp-129-039.wifi.sissa.it>
2020-02-13 10:05:35 +0100

    rem get_a_line

--- src/utilities.c
+++ src/utilities.c
@@ -1,38 +1,4 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-#include "../include/ljmod.h"
-
-/* helper function: read a line and then return
-   the first string with whitespace stripped off */
-int get_a_line(FILE *fp, char *buf) {
-  char tmp[BLEN], *ptr;
-
-  /* read a line and cut of comments and blanks */
-  if (fgets(tmp, BLEN, fp)) {
-    int i;
-
-    ptr = strchr(tmp, '#');
-    if (ptr) *ptr = '\0';
-    i = strlen(tmp);
-    --i;
-    while (isspace(tmp[i])) {
-      tmp[i] = '\0';
-      --i;
-    }
-    ptr = tmp;
-    while (isspace(*ptr)) {
-      ++ptr;
-    }
-    i = strlen(ptr);
-    strcpy(buf, tmp);
-    return 0;
-  } else {
-    perror("problem reading input");
-    return -1;
-  }
-  return 0;
-}
+#include "../include/ljmd.h"
 
 /* helper function: zero out an array */
 void azzero(double *d, const int n) {
