77421a698db0e61abd17901778db431651538416
Luis Leon <lleon95@gmail.com>
2020-02-13 16:23:40 +0100

    Accept dynamic test cases

--- tests/unit-test-c-ekin/unit-test-c-ekin.c
+++ tests/unit-test-c-ekin/unit-test-c-ekin.c
@@ -17,33 +17,57 @@
 /* Import all the constants and references */
 #include "unit-test-c-ekin.h"
 
+/* 
+  In this unit test we have the following DoF:
+  1. Atom mass given in AUM
+  2. Velocities in X, Y, Z
+  3. Number of atoms (with a singularity in one)
+
+  -----
+  kboltz and mvsq2 are kept constant
+*/
+
 int main() {
   int err = 0;
 
   /* Create test object */
   mdsys_t SUT;
 
-  /* Fill the System Under Test with details */
-  SUT.natoms = NATOMS;
-  SUT.mass = MASS;
-  SUT.vx = (double *) malloc(NATOMS * sizeof(double));
-  SUT.vy = (double *) malloc(NATOMS * sizeof(double));
-  SUT.vz = (double *) malloc(NATOMS * sizeof(double));
-
   /* Fill the velocities */
-  int i;
-  for(i = 0; i < NATOMS; ++i) {
-    SUT.vx[i] = LUT_vel[i][0];
-    SUT.vx[i] = LUT_vel[i][1];
-    SUT.vx[i] = LUT_vel[i][2];
+  int n;
+  for (n = 0; n < n_test_cases; ++n) {
+    int i;
+    
+    SUT.natoms = test_cases[n].natoms;
+    SUT.mass = test_cases[n].mass;
+
+    SUT.vx = (double *) malloc(SUT.natoms * sizeof(double));
+    SUT.vy = (double *) malloc(SUT.natoms * sizeof(double));
+    SUT.vz = (double *) malloc(SUT.natoms * sizeof(double));
+
+    for(i = 0; i < test_cases[n].natoms; ++i) {
+      SUT.vx[i] = test_cases[n].velocities[i][0];
+      SUT.vy[i] = test_cases[n].velocities[i][1];
+      SUT.vz[i] = test_cases[n].velocities[i][2];
+    }
+
+    /* Compute result */
+    ekin(&SUT);
+
+    free(SUT.vx);
+    free(SUT.vy);
+    free(SUT.vz);
+
+    /* Verify */
+    if(abs(SUT.ekin - test_cases[n].exp_ekin) > EPS || 
+        abs(SUT.temp - test_cases[n].exp_temp) > EPS) {
+      err += 1;
+      printf("Test Case(%s): %u - FAILED\n", test_cases[n].case_name, n);  
+      printf("Got: %15.10f %15.10f, Expected: %15.10f %15.10f\n", SUT.ekin, SUT.temp, test_cases[n].exp_ekin, test_cases[n].exp_temp);
+    } else {
+      printf("Test Case(%s): %u - PASSED\n", test_cases[n].case_name, n);  
+    }
   }
-
-  /* Compute result */
-  ekin(&SUT);
-
-  /* Verify */
-  err += abs(SUT.ekin - EXPECTED_EKIN) > EPS;
-  err += abs(SUT.temp - EXPECTED_TEMP) > EPS;
-
+  
   return err;
 }
