902f73ce2fe2e5ef3823b8b42f93c6e896f07acf
Jazz <kristerjazz.urog@gmail.com>
2020-02-13 18:57:27 +0100

    Finalize unit test D for input

--- tests/unit-test-d-input/unit-test-d-input.c
+++ tests/unit-test-d-input/unit-test-d-input.c
@@ -5,46 +5,85 @@ Contributor - Krister Jazz Urog
 
 */
 #include "unit-test-d-input.h"
+
+#include <stdlib.h>
+
 #include "../include/ljmd.h"
 
-#define TEST_SUCESS 0
+#define TEST_SUCCESS 0
 #define TEST_FAILED -1
 
+int test_equality(double in, double test_value) {
+  if (in == test_value)
+    return 1;
+  else
+    return 0;
+}
+
 int main(int argc, char **argv) {
+  char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
+  // int testing_progress=0;
 
   /* read input file */
+  puts("============BEGIN TESTS=============");
+  printf("Reading number of atoms: ");
   if (get_a_line(stdin, line)) return 1;
-  sys.natoms = atoi(line);
+  int natoms = atoi(line);
+  if (test_equality(natoms, NATOMS)) printf("Test success\n");
 
+  printf("Reading mass of atoms: ");
   if (get_a_line(stdin, line)) return 1;
-  sys.mass = atof(line);
+  double mass = atof(line);
+  if (test_equality(mass, MASS)) printf("Test success\n");
 
+  printf("Reading epsilon: ");
   if (get_a_line(stdin, line)) return 1;
-  sys.epsilon = atof(line);
+  double epsilon = atof(line);
+  if (test_equality(epsilon, EPSILON)) printf("Test success\n");
 
+  printf("Reading sigma: ");
   if (get_a_line(stdin, line)) return 1;
-  sys.sigma = atof(line);
+  double sigma = atof(line);
+  if (test_equality(sigma, SIGMA)) printf("Test success\n");
 
+  printf("Reading RCUT: ");
   if (get_a_line(stdin, line)) return 1;
-  sys.rcut = atof(line);
+  double rcut = atof(line);
+  if (test_equality(rcut, RCUT)) printf("Test success\n");
 
+  printf("Reading BOX LENGTH: ");
   if (get_a_line(stdin, line)) return 1;
-  sys.box = atof(line);
+  double box = atof(line);
+  if (test_equality(box, BOX)) printf("Test success\n");
 
+  printf("Reading RESTFILE: ");
   if (get_a_line(stdin, restfile)) return 1;
+  printf("Test success\n");
 
+  printf("Reading TRAJFILE: ");
   if (get_a_line(stdin, trajfile)) return 1;
+  printf("Test success\n");
 
+  printf("Reading ERGFILE: ");
   if (get_a_line(stdin, ergfile)) return 1;
+  printf("Test success\n");
 
+  printf("Reading NSTEPS: ");
   if (get_a_line(stdin, line)) return 1;
-  sys.nsteps = atoi(line);
+  int nsteps = atoi(line);
+  if (test_equality(nsteps, NSTEPS)) printf("Test success\n");
 
+  printf("Reading DT: ");
   if (get_a_line(stdin, line)) return 1;
-  sys.dt = atof(line);
+  double dt = atof(line);
+  if (test_equality(dt, DT)) printf("Test success\n");
 
+  printf("Reading NPRINT: ");
   if (get_a_line(stdin, line)) return 1;
   int nprint = atoi(line);
+  if (test_equality(nprint, NPRINT)) printf("Test success\n");
+
+  puts("============END TESTS=============");
 
   return 0;
 }
