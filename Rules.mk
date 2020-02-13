e109bf0da2be9ed298f0eccd053ce173a2bf80e5
Luis Leon <lleon95@gmail.com>
2020-02-13 19:41:06 +0100

    Factorise paths out to Rules.mk

--- Rules.mk
+++ Rules.mk
@@ -4,7 +4,13 @@ ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 
 # Paths
 LIB_PATH=$(ROOT_DIR)/lib
+SRC_PATH=$(ROOT_DIR)/src
+HEADER_PATH=$(ROOT_DIR)/include
 EXE_PATH=$(ROOT_DIR)
 
 # File names
 EXE_FILE=$(PROJECT_NAME).x
+EXE=$(EXE_PATH)/$(EXE_FILE)
+
+SHARED_FILE=lib$(PROJECT_NAME).so
+SHARED=$(LIB_PATH)/$(SHARED_FILE)
