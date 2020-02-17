53ab593ddc667f897c2f0139972bec6f06c6b653
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-02-17 19:17:20 +0100

    Rebase to develop

--- src/force.c
+++ src/force.c
@@ -19,7 +19,7 @@
 #define POW12(x) (POW6(x) * POW6(x))
 
 /* helper function: apply minimum image convention */
-double pbc(double x, const double boxby2) {
+inline double pbc(double x, const double boxby2) {
   while (x > boxby2) x -= 2.0 * boxby2;
   while (x < -boxby2) x += 2.0 * boxby2;
   return x;
