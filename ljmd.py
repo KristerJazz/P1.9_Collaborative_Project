63d75fa2c0ea121884362e2f738390f7a85232e7
Jazz <kristerjazz.urog@gmail.com>
2020-02-17 11:16:52 +0100

    Add class mdsys_t based from struct mdsys_t

--- ljmd.py
+++ ljmd.py
@@ -3,4 +3,21 @@ from ctypes import *
 so_files = "lib/libljmd.so"
 
 my_functions = CDLL(so_files)
-print(my_functions)
+
+def read_input(datafile):
+	with open(datafile) as f:
+		data = f.read()
+		print(data)
+
+
+class mdsys_t(Structure):
+	_fields_ = [('natoms', c_int), ('nfi', c_int), ('nsteps', c_int),
+				('dt', c_double), ('mass', c_double), ('epsilon', c_double),
+				('sigma', c_double), ('box', c_double), ('rcut', c_double),
+				('ekin', c_double), ('epot', c_double), ('temp', c_double), 
+				('rx', POINTER(c_double)), ('ry', POINTER(c_double)), ('rz', POINTER(c_double)), 
+				('vx', POINTER(c_double)), ('vy', POINTER(c_double)), ('vz', POINTER(c_double)), 
+				('fx', POINTER(c_double)), ('fy', POINTER(c_double)), ('fz', POINTER(c_double))]
+
+read_input("examples/argon_108.inp")
+#my_functions.mdsys_t
