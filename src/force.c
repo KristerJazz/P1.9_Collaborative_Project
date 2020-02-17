131b5efb48946cddaaaa193d3371bac4c3b69aef
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2020-02-17 12:35:15 +0100

    Revert third law optimization

--- src/force.c
+++ src/force.c
@@ -39,8 +39,11 @@ void force(mdsys_t *sys) {
   double c12 = 4.0 * sys->epsilon * POW12(sys->sigma);
   double c6 = 4.0 * sys->epsilon * POW6(sys->sigma);
   double rcsq = POW2(sys->rcut);
-  for (i = 0; i < (sys->natoms) - 1; ++i) {
-    for (j = i + 1; j < (sys->natoms); ++j) {
+
+  for (i = 0; i < (sys->natoms); ++i) {
+    for (j = 0; j < (sys->natoms); ++j) {
+
+      if (i == j) continue;
 
       /* get distance between particle i and j */
       rx = pbc(sys->rx[i] - sys->rx[j], 0.5 * sys->box);
@@ -55,9 +58,9 @@ void force(mdsys_t *sys) {
         ffac = (12.0 * c12 * rinv6 - 6.0 * c6) * rinv6 * rinv2;
         sys->epot += rinv6 * (c12 * rinv6 - c6); 
 
-        sys->fx[i] += rx * ffac; sys->fx[j] -= rx * ffac;
-        sys->fy[i] += ry * ffac; sys->fy[j] -= ry * ffac;
-        sys->fz[i] += rz * ffac; sys->fz[j] -= rz * ffac;
+        sys->fx[i] += rx * ffac; 
+        sys->fy[i] += ry * ffac; 
+        sys->fz[i] += rz * ffac; 
       }
     }
   }
