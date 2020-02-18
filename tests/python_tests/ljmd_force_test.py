398ec8ddee7483af89c177210148a338c7ae1926
Jazz <kristerjazz.urog@gmail.com>
2020-02-18 15:55:02 +0100

    Add force test inside the cutoff region

--- tests/python_tests/ljmd_force_test.py
+++ tests/python_tests/ljmd_force_test.py
@@ -6,13 +6,19 @@ class LjmdForceTest(unittest.TestCase):
         so_path = "lib/libljmd.so"
         self.main = LJMD(so_path)
 
-    def test_force_correct(self):
-        input_path = "examples/argon_108.inp"
+    def test_force_correct_x(self):
+        input_path = "tests/python_tests/test_inputs/test1.inp"
         self.main.initialize_system(input_path)
-        print(self.main.sys.fx[0])
-        raise
-	
 
-    def test_force_correct_0_outside_1(self):
-        correct_force = 0
-        #assert(self.main.sys
+        self.assertAlmostEqual(1, self.main.sys.fx[0], 7)
+
+    def test_force_correct_y(self):
+        input_path = "tests/python_tests/test_inputs/test2.inp"
+        self.main.initialize_system(input_path)
+        self.assertAlmostEqual(1, self.main.sys.fy[0], 7)
+
+    def test_force_correct_z(self):
+        input_path = "tests/python_tests/test_inputs/test3.inp"
+        self.main.initialize_system(input_path)
+        self.assertAlmostEqual(1, self.main.sys.fz[0], 7)
+
