9680dd99c6c0571a035d9f805036e08bbabf4fd3
Jazz <kristerjazz.urog@gmail.com>
2020-02-18 16:01:02 +0100

    Add test for force function outside the cutoff threshold

--- tests/python_tests/ljmd_force_test.py
+++ tests/python_tests/ljmd_force_test.py
@@ -6,19 +6,35 @@ class LjmdForceTest(unittest.TestCase):
         so_path = "lib/libljmd.so"
         self.main = LJMD(so_path)
 
-    def test_force_correct_x(self):
+    def test_force_correct_x_inside(self):
         input_path = "tests/python_tests/test_inputs/test1.inp"
         self.main.initialize_system(input_path)
 
         self.assertAlmostEqual(1, self.main.sys.fx[0], 7)
 
-    def test_force_correct_y(self):
+    def test_force_correct_y_inside(self):
         input_path = "tests/python_tests/test_inputs/test2.inp"
         self.main.initialize_system(input_path)
         self.assertAlmostEqual(1, self.main.sys.fy[0], 7)
 
-    def test_force_correct_z(self):
+    def test_force_correct_z_inside(self):
         input_path = "tests/python_tests/test_inputs/test3.inp"
         self.main.initialize_system(input_path)
         self.assertAlmostEqual(1, self.main.sys.fz[0], 7)
+
+    def test_force_correct_x_outside(self):
+        input_path = "tests/python_tests/test_inputs/test4.inp"
+        self.main.initialize_system(input_path)
+
+        self.assertAlmostEqual(0, self.main.sys.fx[0], 7)
+
+    def test_force_correct_y_outside(self):
+        input_path = "tests/python_tests/test_inputs/test5.inp"
+        self.main.initialize_system(input_path)
+        self.assertAlmostEqual(0, self.main.sys.fy[0], 7)
+
+    def test_force_correct_z_outside(self):
+        input_path = "tests/python_tests/test_inputs/test6.inp"
+        self.main.initialize_system(input_path)
+        self.assertAlmostEqual(0, self.main.sys.fz[0], 7)
 
