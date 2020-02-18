9e63eaf66de0f7ecef3c115a1f95e5f2886cb231
Jazz <kristerjazz.urog@gmail.com>
2020-02-18 17:55:37 +0100

    Add initialize system unit test

---
+++ tests/python_tests/initialize_system_test.py
@@ -0,0 +1,25 @@
+import unittest
+from ljmd import *
+import os
+
+class SystemInitTest(unittest.TestCase):
+    def setUp(self):
+        so_path = "lib/libljmd.so"
+        self.main = LJMD(so_path)
+        os.environ["EXAMPLES_PATH"] = "tests/python_tests/test_inputs/"
+
+    def test_initialize_system_correct(self):
+        correct_file = "tests/python_tests/test_inputs/test1.inp"
+        self.main.initialize_system(correct_file)
+        """
+		correct_file = "examples/argon_108.inp"
+        correct_file = "examples/argon_108.inp"
+        correct_file = "examples/argon_108.inp"
+        correct_file = "examples/argon_108.inp"
+        correct_file = "examples/argon_108.inp"
+        correct_file = "examples/argon_108.inp"
+        correct_file = "examples/argon_108.inp"
+		"""
+    def test_wrong_input_values(self):
+        wrong_data_in_file = "tests/python_tests/wrong_input.inp"
+        self.assertRaises(ValueError, self.main.initialize_system, wrong_data_in_file)
