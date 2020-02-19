b9330378e363cd02538c971551dd85dbf192a51b
Mattia Carello <matrixc@dhcp-129-012.wifi.sissa.it>
2020-02-19 11:33:28 +0100

    ptr guards

--- src/velverlet_step.c
+++ src/velverlet_step.c
@@ -10,16 +10,20 @@
 
 #include "ljmd.h"
 
-inline void propagate_position(mdsys_t *sys, int i) {
+inline int propagate_position(mdsys_t *sys, int i) {
+  if(!sys) return -1;
   sys->rx[i] += sys->dt * sys->vx[i];
   sys->ry[i] += sys->dt * sys->vy[i];
   sys->rz[i] += sys->dt * sys->vz[i];
+  return 0;
 }
 
-inline void propagate_velocity(mdsys_t *sys, int i) {
+inline int propagate_velocity(mdsys_t *sys, int i) {
+  if(!sys) return -1;
   sys->vx[i] += 0.5 * sys->dt / mvsq2e * sys->fx[i] / sys->mass;
   sys->vy[i] += 0.5 * sys->dt / mvsq2e * sys->fy[i] / sys->mass;
   sys->vz[i] += 0.5 * sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+  return 0;
 }
 
 void initial_propagation(mdsys_t *sys) {
